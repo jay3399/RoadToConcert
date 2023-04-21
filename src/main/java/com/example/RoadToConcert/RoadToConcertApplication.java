d57f328d2e30d40b49104fe183abe3b4c93a8ae1
jay <joswxx@gmail.com>
2023-04-21 21:01:39 +0900

    ConfigServer

--- src/main/java/com/example/RoadToConcert/RoadToConcertApplication.java
+++ src/main/java/com/example/RoadToConcert/RoadToConcertApplication.java
@@ -2,8 +2,10 @@ package com.example.RoadToConcert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RoadToConcertApplication {
 
 	public static void main(String[] args) {
