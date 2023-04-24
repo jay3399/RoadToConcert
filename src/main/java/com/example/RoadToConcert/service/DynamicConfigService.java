26f665f6774f96511fc4d8fbcca50638d429f352
jay <joswxx@gmail.com>
2023-04-24 18:29:18 +0900

    ConfigServer

--- src/main/java/com/example/RoadToConcert/service/DynamicConfigService.java
+++ src/main/java/com/example/RoadToConcert/service/DynamicConfigService.java
@@ -1,34 +1,34 @@
-package com.example.RoadToConcert.service;
-
-
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Service;
-
-@Service
-@RefreshScope
-public class DynamicConfigService {
-
-  @Value("${my.name}")
-  private String name;
-
-  @Value("${my.age}")
-  private String age;
-
-
-  public Map<String, String> getConfig() {
-
-    Map<String, String> map = new HashMap<>();
-
-    map.put("name", name);
-    map.put("age", age);
-
-    return map;
-
-
-  }
-
-
-}
+//package com.example.RoadToConcert.service;
+//
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RefreshScope
+//public class DynamicConfigService {
+//
+//  @Value("${my.name}")
+//  private String name;
+//
+//  @Value("${my.age}")
+//  private String age;
+//
+//
+//  public Map<String, String> getConfig() {
+//
+//    Map<String, String> map = new HashMap<>();
+//
+//    map.put("name", name);
+//    map.put("age", age);
+//
+//    return map;
+//
+//
+//  }
+//
+//
+//}
